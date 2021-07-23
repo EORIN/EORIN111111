1d7ccda390e46e92f2c69b4895aa12448ac48aa5
EORIN <73447841+EORIN@users.noreply.github.com>
2021-07-23 15:43:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// $('h1').click(()=>{
+//     $.ajax({
+//     type: "POST",
+//     url: 'http://localhost:3000/mails',
+//     data: `{je}`,
+//     success: console.log('ok'),
+//     dataType: 'json',
+//     headers: {'Content-Type': 'application/JSON'}
+//   })
+
+//   $('h1').html(`kek`)
+// })
